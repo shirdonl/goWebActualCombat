9d85b0c867f93d75d73b8544dd55b3fecac055e5
shirdonl <hcbsts@163.com>
2021-01-19 21:25:38 +0800

    commit

--- chapter1/1.4-pointer.go
+++ chapter1/1.4-pointer.go
@@ -1,3 +1,11 @@
+//++++++++++++++++++++++++++++++++++++++++
+// 《Go Web编程实战派从入门到精通》源码
+//++++++++++++++++++++++++++++++++++++++++
+// Author:廖显东（ShirDon）
+// Blog:https://www.shirdon.com/
+// 仓库地址：https://gitee.com/shirdonl/goWebActualCombat
+// 仓库地址：https://github.com/shirdonl/goWebActualCombat
+//++++++++++++++++++++++++++++++++++++++++
 package main
 
 func main() {
