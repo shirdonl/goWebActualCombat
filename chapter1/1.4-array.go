59e6fc9c035e0548c025d9413f09039a9ee433f7
shirdonl <hcbsts@163.com>
2021-04-23 14:15:05 +0800

    commit

--- chapter1/1.4-array.go
+++ chapter1/1.4-array.go
@@ -11,12 +11,12 @@ package main
 import "fmt"
 
 func main() {
-	var arr [6]int /* n 是一个长度为 5 的数组 */
+	var arr [6]int // arr 是一个长度为 6 的数组
 	var i, j int
-	for i = 0; i < 6; i++ { /* 为数组 n 初始化元素 */
-		arr[i] = i + 66 /* 设置元素为 i + 66 */
+	for i = 0; i < 6; i++ { // 为数组 arr 初始化元素
+		arr[i] = i + 66 // 设置元素为 i + 66
 	}
-	for j = 0; j < 6; j++ { /* 输出每个数组元素的值 */
+	for j = 0; j < 6; j++ { // 输出每个数组元素的值
 		fmt.Printf("Array[%d] = %d\n", j, arr[j])
 	}
 }
