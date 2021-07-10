7aea9ec4ab140c0b8c2866a101849dc3e27e6d99
shirdonl <hcbsts@163.com>
2021-07-10 09:46:33 +0800

    update

--- chapter1/1.8-reflection4.go
+++ chapter1/1.8-reflection4.go
@@ -17,7 +17,6 @@ func main() {
 	var name string = "Go Web Program"
 	v1 := reflect.ValueOf(&name)
 	fmt.Println("v1 可写性为:", v1.CanSet())
-
 	v2 := v1.Elem()
 	fmt.Println("v2 可写性为:", v2.CanSet())
 }
