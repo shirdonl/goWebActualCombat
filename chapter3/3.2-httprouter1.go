313b7aa9db46c8466c611c92f4271d50759ec56c
shirdonl <hcbsts@163.com>
2021-11-03 14:48:51 +0800

    Fix

--- chapter3/3.2-httprouter1.go
+++ chapter3/3.2-httprouter1.go
@@ -23,9 +23,9 @@ func main() {
 		w.Write([]byte("default post"))
 	})
 	//精确匹配
-	router.GET("/user/name", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		w.Write([]byte("user name:" + p.ByName("name")))
-	})
+	//router.GET("/user/:name", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	//	w.Write([]byte("user name:" + p.ByName("name")))
+	//})
 	//匹配所有
 	router.GET("/user/*name", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		w.Write([]byte("user name:" + p.ByName("name")))
